1af50d0e11adb5e0da4c2f2c9e90e8961c676b69
kailinlu <kl2978@columbia.edu>
2018-10-21 15:46:18 -0400

    record from microphone

--- assets/js/recordapp.js
+++ assets/js/recordapp.js
@@ -1,116 +1,123 @@
-                                // CODE FROM https://addpipe.com/blog/using-recorder-js-to-capture-wav-audio-in-your-html5-web-site/
+// CODE REFERENCED FROM
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
+// https://developers.google.com/web/fundamentals/media/recording-audio/
+// https://github.com/addpipe/Media-Recorder-API-Demo/blob/master/js/main.js
+// https://github.com/mdn/web-dictaphone
 
-//webkitURL is deprecated but nevertheless
-URL = window.URL || window.webkitURL;
+navigator.getUserMeia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-var gumStream; //stream from getUserMedia()
-var rec; //Recorder.js object
-var input; //MediaStreamAudioSourceNode we'll be recording
+var constraints = { audio: true }
 
-// shim for AudioContext when it's not avb.
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext = new AudioContext; //new audio context to help us record
+var record = document.querySelector('button#recordButton');
+var stop= document.querySelector('button#stopButton');
+var canvas = document.getElementById('visualizer');
+var startDrawing = true;
+WIDTH = canvas.width;
+HEIGHT = canvas.height;
 
-var recordButton = document.getElementById("recordButton");
-var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
+var audioCtx = new (window.AudioContext || websiteAudioContext)();
+var canvasCtx = canvas.getContext('2d');
 
-//add events to those 3 buttons
-recordButton.addEventListener("click", startRecording);
-stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
+function errorCallback(error){
+	console.log('navigator.getUserMedia error: ', error);
+}
 
-function startRecording() {
-    console.log("recordButton clicked");
+var mediaRecorder;
+var chunks = [];
+var count = 0;
 
-    /*
-    Simple constraints object, for more advanced audio features see
-    <div class="video-container"><blockquote class="wp-embedded-content" data-secret="vNsz0nPBL4"><a href="https://addpipe.com/blog/audio-constraints-getusermedia/">Supported Audio Constraints in getUserMedia()</a></blockquote><iframe class="wp-embedded-content" sandbox="allow-scripts" security="restricted" style="position: absolute; clip: rect(1px, 1px, 1px, 1px);" src="https://addpipe.com/blog/audio-constraints-getusermedia/embed/#?secret=vNsz0nPBL4" data-secret="vNsz0nPBL4" width="600" height="338" title="“Supported Audio Constraints in getUserMedia()” — Pipe Blog" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe></div>
-    */
+function startRecording(stream) {
+    mediaRecorder = new MediaRecorder(stream);
+    visualize(stream);
+	mediaRecorder.start();
 
-    var constraints = { audio: true, video:false }
+	console.log(mediaRecorder.state);
+	console.log('Recorder started');
 
-    /*
-    Disable the record button until we get a success or fail from getUserMedia()
-    */
+	var url = window.URL || window.webkitURL;
 
-    recordButton.disabled = true;
-    stopButton.disabled = false;
+	mediaRecorder.ondataavailable = function(e) {
+		chunks.push(e.data);
+	};
 
-    /*
-    We're using the standard promise based getUserMedia()
-    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    */
+	mediaRecorder.onerror = function(e){
+		console.log('Error: ', e);
+	};
 
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+	mediaRecorder.onstop = function(){
+        console.log('Recording Stopped');
+		var blob = new Blob(chunks, {type: "audio/webm"});
+		chunks = [];
 
-        /* assign to gumStream for later use */
-        gumStream = stream;
+        var audio = document.createElement('audio');
+        audio.setAttribute('controls', '');
+        var audioURL = window.URL.createObjectURL(blob);
+        audio.src = audioURL;
 
-        /* use the stream */
-        input = audioContext.createMediaStreamSource(stream);
+	};
+}
 
-        /*
-        Create the Recorder object and configure to record mono sound (1 channel)
-        Recording 2 channels  will double the file size
-        */
-        rec = new Recorder(input,{numChannels:1})
+function onRecordClicked (){
+    startDrawing = true;
+    navigator.getUserMedia(constraints, startRecording, errorCallback);
+    record.disabled = true;
+    stop.disabled = false;
+}
 
-        //start the recording process
-        rec.record()
-
-        console.log("Recording started");
-
-    }).catch(function(err) {
-        //enable the record button if getUserMedia() fails
-        recordButton.disabled = false;
-        stopButton.disabled = true;
-    });
+function onStopClicked(){
+	mediaRecorder.stop();
+	startDrawing = false;
+    console.log(mediaRecorder.state);
+	record.disabled = false;
+	stop.disabled = true;
 }
 
 
-function stopRecording() {
-    console.log("stopButton clicked");
+function visualize(stream) {
 
-    //disable the stop button, enable the record too allow for new recordings
-    stopButton.disabled = true;
-    recordButton.disabled = false;
-    pauseButton.disabled = true;
+    if (startDrawing) {
+        var source = audioCtx.createMediaStreamSource(stream);
+        var analyser = audioCtx.createAnalyser();
 
-    //reset button just in case the recording is stopped while paused
-    pauseButton.innerHTML="Pause";
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
 
-    //tell the recorder to stop the recording
-    rec.stop();
+        source.connect(analyser);
+        draw();
 
-    //stop microphone access
-    gumStream.getAudioTracks()[0].stop();
+        function draw() {
+            var drawVisual = requestAnimationFrame(draw);
 
-    //create the wav blob and pass it on to createDownloadLink
-    rec.exportWAV(createDownloadLink);
+            analyser.getByteTimeDomainData(dataArray);
+
+            canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+            canvasCtx.lineWidth = 1;
+            canvasCtx.strokeStyle = 'rgb(77, 129, 170)';
+            canvasCtx.beginPath();
+
+            var sliceWidth = WIDTH * 1.0 / bufferLength;
+            var x = 0;
+            for(var i = 0; i < bufferLength; i++) {
+
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT/2;
+
+            if(i === 0) {
+              canvasCtx.moveTo(x, y);
+            } else {
+              canvasCtx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+          }
+           canvasCtx.lineTo(canvas.width, canvas.height/2);
+           canvasCtx.stroke();
+        };
+    }
 }
 
-function createDownloadLink(blob) {
 
-    var url = URL.createObjectURL(blob);
-    var au = document.createElement('audio');
-    var li = document.createElement('li');
-    var link = document.createElement('a');
 
-    //add controls to the <audio> element
-    au.controls = true;
-    au.src = url;
 
-    //link the a element to the blob
-    link.href = url;
-    link.download = new Date().toISOString() + '.wav';
-    link.innerHTML = link.download;
-
-    //add the new audio and a elements to the li element
-    li.appendChild(au);
-    li.appendChild(link);
-
-    //add the li element to the ordered list
-    recordingsList.appendChild(li);
-}
