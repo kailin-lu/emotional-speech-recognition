ba2b0eea8b78d61b05e3a64c8d084b3b56e08cfb
kailinlu <kl2978@columbia.edu>
2018-10-20 22:15:46 -0400

    convert model to tensorflowjs

--- assets/js/recordapp.js
+++ assets/js/recordapp.js
@@ -1,4 +1,4 @@
-// CODE FROM https://addpipe.com/blog/using-recorder-js-to-capture-wav-audio-in-your-html5-web-site/
+                                // CODE FROM https://addpipe.com/blog/using-recorder-js-to-capture-wav-audio-in-your-html5-web-site/
 
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
@@ -36,7 +36,6 @@ function startRecording() {
 
     recordButton.disabled = true;
     stopButton.disabled = false;
-    pauseButton.disabled = false
 
     /*
     We're using the standard promise based getUserMedia()
@@ -67,23 +66,9 @@ function startRecording() {
         //enable the record button if getUserMedia() fails
         recordButton.disabled = false;
         stopButton.disabled = true;
-        pauseButton.disabled = true
     });
 }
 
-
-function pauseRecording(){
-    console.log("pauseButton clicked rec.recording=",rec.recording );
-    if (rec.recording){
-        //pause
-        rec.stop();
-        pauseButton.innerHTML="Resume";
-    }else{
-        //resume
-        rec.record()
-        pauseButton.innerHTML="Pause";
-    }
-}
 
 function stopRecording() {
     console.log("stopButton clicked");
@@ -104,4 +89,28 @@ function stopRecording() {
 
     //create the wav blob and pass it on to createDownloadLink
     rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+
+    var url = URL.createObjectURL(blob);
+    var au = document.createElement('audio');
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+
+    //add controls to the <audio> element
+    au.controls = true;
+    au.src = url;
+
+    //link the a element to the blob
+    link.href = url;
+    link.download = new Date().toISOString() + '.wav';
+    link.innerHTML = link.download;
+
+    //add the new audio and a elements to the li element
+    li.appendChild(au);
+    li.appendChild(link);
+
+    //add the li element to the ordered list
+    recordingsList.appendChild(li);
 }
